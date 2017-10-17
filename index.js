55a0d11cd89eee2323253213528d4ec41002a59f
Josef Vacek <vacekj@outlook.com>
2017-10-17 15:39:34 +0200

    Updates

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
+const chalk = require('chalk');
 
 const selectors = {
 	startViewerLink: '#main_page_offline > div > div.config_line > div > div > a',
@@ -7,22 +8,26 @@ const selectors = {
 };
 
 const url = 'http://bit.ly/29briww';
-const viewerUrl = 'http://www.websyndic.com/wv3/target.php';
 
 main();
 
 async function main() {
-	const browser = await puppeteer.launch({ headless: false });
+	console.log(chalk.green('Welcome to Websyndicate!'));
+	const browser = await puppeteer.launch({ headless: true });
 	const page = await browser.newPage();
 	await page.goto(url);
 	await page.waitForSelector(selectors.startViewerDiv);
 	await page.click(selectors.startViewerLink);
-	
+
 	// //Stats
 	// setInterval(async () => {
 	// 	let stats = extractStats(await page.content());
 	// 	console.log(`Stats: ${stats.creditsInBank} ${stats.sitesVisitedToday}`);
 	// }, 5000);
+
+	page.on('load', async () => {
+		console.log(chalk.white('Current page: ' + await page.url()));
+	});
 }
 
 function extractStats(html) {
