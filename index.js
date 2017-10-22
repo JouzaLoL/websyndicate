29ca999df7260221a83e70607f02763d9a2ea9eb
Josef Vacek <vacekj@outlook.com>
2017-10-22 12:26:48 +0200

    Update

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const puppeteer = require('puppeteer');
 const chalk = require('chalk');
+const cheerio = require('cheerio');
+const eventToPromise = require('event-to-promise');
 
 const selectors = {
 	startViewerLink: '#main_page_offline > div > div.config_line > div > div > a',
 	startViewerDiv: 'div.titre_12'
 };
 
-const viewer_url = 'http://bit.ly/29briww';
+const viewerURL = 'http://bit.ly/29briww';
 
 main();
 
@@ -19,13 +21,35 @@ main();
 async function main() {
 	console.log(chalk.green('Welcome to Websyndicate!'));
 	const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
-	const page = await browser.newPage();
-	await page.goto(viewer_url);
-	await page.waitForSelector(selectors.startViewerDiv);
-	await page.click(selectors.startViewerLink);
-	await page.waitForNavigation();
-	let pages = await browser.pages();
-	page.on('load', async () => {
-		console.log(chalk.white('Current page: ' + await page.url()));
+	const viewerPage = await browser.newPage();
+	await viewerPage.goto(viewerURL);
+	await viewerPage.waitForSelector(selectors.startViewerDiv);
+	await viewerPage.click(selectors.startViewerLink);
+	await eventToPromise(browser, 'targetcreated');
+
+	const pages = await browser.pages();
+	const statsPage = pages[2];
+
+	statsPage.on('response', async () => {
+		const stats = parseStatsPage(await statsPage.content());
+		const log_text = `Current page: ${stats.currentPage} | Site stats: day ${stats.sitesDay} - week: ${stats.sitesWeek} - month: ${stats.sitesMonth}`;
+		console.log(log_text);
 	});
+}
+
+function parseStatsPage(html) {
+	const $ = cheerio.load(html);
+
+	const currentPage = $('#last_site_url > a').text();
+
+	const sitesDay = $('#user_cj').text();
+	const sitesWeek = $('#user_cs').text();
+	const sitesMonth = $('#user_cm').text();
+
+	return {
+		currentPage,
+		sitesDay,
+		sitesWeek,
+		sitesMonth
+	};
 }
