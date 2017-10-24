a9a5cfd515614b9a4bbd56c469a6e256fca18419
Josef Vacek <vacekj@outlook.com>
2017-10-24 21:09:18 +0200

    Hard reset on page crash doesn't work

--- restart.js
+++ restart.js
@@ -3,7 +3,13 @@ const URL = 'https://api.heroku.com/apps/websyndicate/dynos';
 const Heroku = require('heroku-client');
 const heroku = new Heroku({ token: process.env.TOKEN });
 
-heroku
-	.delete(URL)
-	.then(app => { console.log('! Dynos restarted succesfully'); })
-	.catch(err => console.log(err));
+restart();
+
+function restart() {
+	heroku
+		.delete(URL)
+		.then(app => { console.log('! Dynos restarted succesfully'); })
+		.catch(err => console.log(err));
+}
+
+module.exports = restart;
