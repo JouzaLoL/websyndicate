32b012e07fbd7056731fd8d6dcf0922ff4983816
Josef Vacek <vacekj@outlook.com>
2017-10-24 19:29:07 +0200

    Add restart script for heroku scheduler

---
+++ restart.js
@@ -0,0 +1,9 @@
+const URL = 'https://api.heroku.com/apps/websyndicate/dynos';
+
+const Heroku = require('heroku-client');
+const heroku = new Heroku({ token: process.env.TOKEN });
+
+heroku
+	.delete(URL)
+	.then(app => { console.log('! Dynos restarted succesfully'); })
+	.catch(err => console.log(err));
