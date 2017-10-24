33e7d62284fc070a4fa4ea135d46f422d2c94667
Josef Vacek <vacekj@outlook.com>
2017-10-24 15:18:31 +0200

    Implement log throttling

--- index.js
+++ index.js
@@ -32,13 +32,11 @@ async function main() {
 	const statsPage = pages[2];
 
 	statsPage.on('response', async () => {
-		const stats = parseStatsPage(await statsPage.content());
-		const log_text = `Current page: ${stats.currentPage} | Site stats: day ${stats.sitesDay} - week: ${stats.sitesWeek} - month: ${stats.sitesMonth}`;
-		console.log(log_text);
+		logStats(await statsPage.content());
 	});
 
-	statsPage.on('error', async () => await restart(browser));
-	viewerPage.on('error', async () => await restart(browser));
+	statsPage.on('error', () => restart(browser));
+	viewerPage.on('error', () => restart(browser));
 }
 
 async function restart(browser) {
@@ -63,3 +61,41 @@ function parseStatsPage(html) {
 		sitesMonth
 	};
 }
+
+var logStats = throttle(function (statsPageContent) {
+	const stats = parseStatsPage(statsPageContent);
+	const log_text = `Current page: ${stats.currentPage} | Site stats: day: ${stats.sitesDay} - week: ${stats.sitesWeek} - month: ${stats.sitesMonth}`;
+	console.log(log_text);
+});
+
+function throttle(func, wait, options) {
+	var context, args, result;
+	var timeout = null;
+	var previous = 0;
+	if (!options) options = {};
+	var later = function () {
+		previous = options.leading === false ? 0 : Date.now();
+		timeout = null;
+		result = func.apply(context, args);
+		if (!timeout) context = args = null;
+	};
+	return function () {
+		var now = Date.now();
+		if (!previous && options.leading === false) previous = now;
+		var remaining = wait - (now - previous);
+		context = this;
+		args = arguments;
+		if (remaining <= 0 || remaining > wait) {
+			if (timeout) {
+				clearTimeout(timeout);
+				timeout = null;
+			}
+			previous = now;
+			result = func.apply(context, args);
+			if (!timeout) context = args = null;
+		} else if (!timeout && options.trailing !== false) {
+			timeout = setTimeout(later, remaining);
+		}
+		return result;
+	};
+};
